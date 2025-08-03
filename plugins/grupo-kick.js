2693da09af4db305f1fb9332a6261aa325454f9f
Its.mia.oficial <hibqbqxdd@gmail.com>
2025-08-03 00:13:16 -0300

    Create grupo-kick.js

---
+++ plugins/grupo-kick.js
@@ -0,0 +1,20 @@
+let handler = async (m, { conn, isAdmin, isBotAdmin, text }) => {
+  if (!m.isGroup) throw 'Este comando solo funciona en grupos.';
+  if (!isAdmin) throw 'Solo los administradores pueden usar este comando.';
+  if (!isBotAdmin) throw 'Necesito ser administrador para ejecutar eso.';
+
+  let user = m.mentionedJid[0] || text;
+  if (!user) throw 'Menciona o escribe el número del usuario a expulsar.';
+
+  await conn.groupParticipantsUpdate(m.chat, [user], 'remove');
+  await m.reply('✅ Usuario expulsado.');
+};
+
+handler.help = ['kick @usuario'];
+handler.tags = ['admin'];
+handler.command = /^kick$/i;
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
+
+export default handler;
