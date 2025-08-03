68a5dcd31d70db699d22ca0d69de4e8f3e14b022
Its.mia.oficial <hibqbqxdd@gmail.com>
2025-08-03 00:11:11 -0300

    Create grupo-open.js

---
+++ plugins/grupo-open.js
@@ -0,0 +1,16 @@
+let handler = async (m, { conn, isAdmin, isBotAdmin }) => {
+  if (!m.isGroup) throw 'Este comando solo funciona en grupos.';
+  if (!isAdmin) throw 'Solo los administradores pueden usar este comando.';
+  if (!isBotAdmin) throw 'Necesito ser administrador para ejecutar eso.';
+
+  await conn.groupSettingUpdate(m.chat, 'not_announcement');
+  await m.reply('✅ El grupo ha sido *abierto* .');
+};
+
+handler.help = ['open'];
+handler.tags = ['admin'];
+handler.command = /^open$/i;
+handler.group = true;
+handler.admin = true;
+
+export default handler;
